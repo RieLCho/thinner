78d2c882e8d1ad4e0c8ef42e47b0514fc69725a0
Yangjin Cho <choyj1222@gmail.com>
2023-04-08 21:38:30 +0900

    feat: implement ElementsWithScrolls

--- thinner.js
+++ thinner.js
@@ -1,8 +1,34 @@
-htmlTags = document.getElementsByTagName("html")
-for(var i=0; i < htmlTags.length; i++) {
-    console.log(htmlTags[i]);
-    console.log(htmlTags[i].style);
-    console.log(htmlTags[i].style.scrollBarWidth);
-    htmlTags[i].style.scrollBarWidth = "thin";
-    console.log(htmlTags[i].style.scrollBarWidth);
-}
+var ElementsWithScrolls = (function() {
+    var getComputedStyle = document.body && document.body.currentStyle ? function(elem) {
+        return elem.currentStyle;
+    } : function(elem) {
+        return document.defaultView.getComputedStyle(elem, null);
+    };
+
+    function getActualCss(elem, style) {
+        return getComputedStyle(elem)[style];
+    }
+
+    function isXScrollable(elem) {
+        return elem.offsetWidth < elem.scrollWidth &&
+            autoOrScroll(getActualCss(elem, 'overflow-x'));
+    }
+
+    function isYScrollable(elem) {
+        return elem.offsetHeight < elem.scrollHeight &&
+            autoOrScroll(getActualCss(elem, 'overflow-y'));
+    }
+
+    function autoOrScroll(text) {
+        return text == 'scroll' || text == 'auto';
+    }
+
+    function hasScroller(elem) {
+        return isYScrollable(elem) || isXScrollable(elem);
+    }
+    return function ElemenetsWithScrolls() {
+        return [].filter.call(document.querySelectorAll('*'), hasScroller);
+    };
+})();
+
+console.log(ElementsWithScrolls());
